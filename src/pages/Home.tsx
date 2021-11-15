a9719ef1d8fba6b304a3cde657ed7b56bade8c84
Thang Le Dinh <thangld2407@gmail.com>
2021-11-16 04:31:14 +0700

    update new validate

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -2,15 +2,12 @@ import { IonButton, IonContent, IonDatetime, IonHeader, IonInput, IonItem, IonLa
 import { useState } from 'react';
 import './Home.css';
 
+export function isEmptyorWhiteSpace(value: string) {
+  const re = /^\s*$/;
+  return re.test(value);
+}
 
-
-export function validateEmptyOrWhiteSpace(value: string) {
-    const re = /^\s*$/;
-    return re.test(value);
-  }
-
-
-export function validateNumber(value: string) {
+export function isNumber(value: string) {
   const re = /^\d+$/;
   return re.test(value);
 }
@@ -28,20 +25,13 @@ function convertDate(date: string) {
   const newDate = new Date(date);
   const year = newDate.getFullYear();
   const month = (newDate.getMonth() + 1);
-  const day = newDate.getDay();
-  
+  const day = newDate.getDate();
 
-  return joinDate(year, month, day);
+
+  return formatDateYearMonthDay(year, month, day);
 };
 
-/**
- * Join year, month, date
- * @param year 
- * @param month 
- * @param date 
- * @returns YYYY/M/D
- */
-function joinDate(year: number, month: number, date: number) {
+function formatDateYearMonthDay(year: number, month: number, date: number) {
   if (!year && !month && !date) {
     return '';
   }
@@ -49,78 +39,72 @@ function joinDate(year: number, month: number, date: number) {
   return `${year}/${month}/${date}`;
 };
 
-/**
- * Validate Form
- * @param Form 
- * @returns Array item error
- */
-
-function validateForm(Form:any){
+function validatFormApartment(formApartment: any) {
   const listHouse = ['Flat', 'House', 'Bungalow'];
   const listBedrooms = ['Studio', 'One', 'Two'];
   const listFurnitures = ['Furnished', 'Unfurnished', 'PartFunished'];
 
- let isValidate = [];
+  let isValidated = [];
 
- for(const key in Form ) {
-   if(Object.prototype.hasOwnProperty.call(Form, key)){
-     const el = Form[key];
-     switch (key) {
+  for (const index in formApartment) {
+    if (Object.prototype.hasOwnProperty.call(formApartment, index)) {
+      const el = formApartment[index];
+      switch (index) {
         case 'rentPrice': {
-          if(!validateNumber(el)){
-            isValidate.push('Rent Price');
-          } 
-           break;
+          if (!isNumber(el)) {
+            isValidated.push('You need to enter monthly rent price');
+          }
+          break;
         }
-        case 'nameReporter':{
-          if(validateEmptyOrWhiteSpace(el)){
-            isValidate.push('Name Reporter');
+        case 'nameReporter': {
+          if (isEmptyorWhiteSpace(el)) {
+            isValidated.push('Name reporter is not empty ');
           }
           break;
         }
         case 'dateTime': {
-          if(!validateDate(el)){
-            isValidate.push('Date Time ')
+          if (!validateDate(el)) {
+            isValidated.push('Please enter date ')
           }
           break;
         }
-        case 'notes':{
-          if(validateEmptyOrWhiteSpace(el)){
-            isValidate.push('Notes');
-          } 
+        case 'notes': {
+          // if(validateEmptyOrWhiteSpace(el)){
+          //   isValidated.push('Notes');
+          // } 
           break;
         }
-        case 'houses':{
-          if(!listHouse.includes(el)){
-            isValidate.push('Houses');
+        case 'houses': {
+          if (!listHouse.includes(el)) {
+            isValidated.push('Type of properties is not recognize');
           }
           break;
         }
-        case 'bedrooms':{
-          if(!listBedrooms.includes(el)){
-            isValidate.push('Bedrooms');
+        case 'bedrooms': {
+          if (!listBedrooms.includes(el)) {
+            isValidated.push('Type of bedrooms is not recognize');
           }
           break;
         }
-        case 'furnitures':{
-          if(!listFurnitures.includes(el)){
-            isValidate.push('Furnitures');
+        case 'furnitures': {
+          if (!listFurnitures.includes(el)) {
+            isValidated.push('Type of furnitures is not recognize');
           }
           break;
         }
         // default:{
         //   isValidate.push(false);
         // }
-     }
-   }
- }
- return isValidate;
+      }
+    }
+  }
+  return isValidated;
 }
 
-function createMessageError(listError: any) {
-  const message = listError.join(', ');
+function errorMessage(err: any) {
+  const listError = err.join('<br>');
 
-  return `You entered ${message} incorrectly`;
+  return `${listError}`;
 }
 
 
@@ -133,40 +117,40 @@ const Home: React.FC = () => {
   const [bedrooms, setBedrooms] = useState('');
   const [dateTime, setDateTime] = useState('');
   const [furnitures, setFurnitures] = useState('');
-  
+
   const [showToast, setShowToast] = useState(false);
   const [message, setMessage] = useState('');
   const [colorMessage, setColorMessage] = useState('');
 
   const handleSubmit = () => {
-    const Form = {
+    const formApartment = {
       houses,
-      bedrooms, 
-      dateTime:convertDate(dateTime), 
+      bedrooms,
+      dateTime: convertDate(dateTime),
       furnitures,
       notes,
       nameReporter,
       rentPrice
     }
-    const isValidate:any = validateForm(Form);
-    if(isValidate.length === 0) {
-      setMessage('Successfully' );
+    const isValidate: any = validatFormApartment(formApartment);
+    if (isValidate.length === 0) {
+      setMessage('You are successfully to create a new room');
       setShowToast(true);
       setColorMessage('success');
 
-      setTimeout(()=> {
+      setTimeout(() => {
         setShowToast(false);
       }, 3000)
     } else {
-      setMessage(createMessageError(isValidate));
+      setMessage(errorMessage(isValidate));
       setShowToast(true);
       setColorMessage('warning');
 
-      setTimeout(()=> {
+      setTimeout(() => {
         setShowToast(false);
       }, 3000)
     }
-}
+  }
   return (
     <IonPage>
       <IonHeader>
@@ -178,29 +162,29 @@ const Home: React.FC = () => {
         <div className="ion-padding">
           <IonItem>
             <IonLabel position="stacked">Select House</IonLabel>
-            <IonSelect 
-            value={houses}
-            onIonChange={e =>setHouse(e.detail.value)}
+            <IonSelect
+              value={houses}
+              onIonChange={e => setHouse(e.detail.value)}
             >
-              <IonSelectOption value = "Flat">Flat</IonSelectOption>
-              
-              <IonSelectOption value = "House">House</IonSelectOption>
-              
-              <IonSelectOption value = "Bungalow">Bungalow</IonSelectOption>
-              
+              <IonSelectOption value="Flat">Flat</IonSelectOption>
+
+              <IonSelectOption value="House">House</IonSelectOption>
+
+              <IonSelectOption value="Bungalow">Bungalow</IonSelectOption>
+
             </IonSelect>
           </IonItem>
           <IonItem>
             <IonLabel position="stacked">Select Bedrooms</IonLabel>
             <IonSelect
-            value={bedrooms}
-            onIonChange={e => setBedrooms(e.detail.value  )}
+              value={bedrooms}
+              onIonChange={e => setBedrooms(e.detail.value)}
             >
-              <IonSelectOption value = "Studio">Studio</IonSelectOption>
-              
-              <IonSelectOption value = "One">One</IonSelectOption>
-              
-              <IonSelectOption value = "Two">Two</IonSelectOption>
+              <IonSelectOption value="Studio">Studio</IonSelectOption>
+
+              <IonSelectOption value="One">One</IonSelectOption>
+
+              <IonSelectOption value="Two">Two</IonSelectOption>
 
             </IonSelect>
           </IonItem>
@@ -212,34 +196,34 @@ const Home: React.FC = () => {
 
           <IonItem>
             <IonLabel position="stacked">Select Furnitures</IonLabel>
-            <IonSelect 
-            value={furnitures}
-            onIonChange={e => setFurnitures(e.detail.value)}
+            <IonSelect
+              value={furnitures}
+              onIonChange={e => setFurnitures(e.detail.value)}
             >
-              <IonSelectOption value = "Furnished">Furnished</IonSelectOption>
-              
-              <IonSelectOption value = "Unfurnished">Unfurnished</IonSelectOption>
-              
-              <IonSelectOption value = "PartFunished">Part Furnished</IonSelectOption>
-              
+              <IonSelectOption value="Furnished">Furnished</IonSelectOption>
+
+              <IonSelectOption value="Unfurnished">Unfurnished</IonSelectOption>
+
+              <IonSelectOption value="PartFunished">Part Furnished</IonSelectOption>
+
             </IonSelect>
           </IonItem>
 
           <IonItem>
             <IonLabel position="stacked">Notes</IonLabel>
-            <IonInput placeholder="Notes" onIonChange={e=>setNotes(e.detail.value!)}></IonInput>
+            <IonInput placeholder="Notes" onIonChange={e => setNotes(e.detail.value!)}></IonInput>
           </IonItem>
           <IonItem>
             <IonLabel position="stacked">Reporter</IonLabel>
-            <IonInput placeholder="Reporter" type="text" onIonChange={e=>setNameReporter(e.detail.value!)}></IonInput>
+            <IonInput placeholder="Reporter" type="text" onIonChange={e => setNameReporter(e.detail.value!)}></IonInput>
           </IonItem>
           <IonItem>
             <IonLabel position="stacked" >Rent Price</IonLabel>
-            <IonInput placeholder="Rent Price" type="number" onIonChange={e=>setRentPrice(e.detail.value!)}></IonInput>
+            <IonInput placeholder="Rent Price" type="number" onIonChange={e => setRentPrice(e.detail.value!)}></IonInput>
           </IonItem>
-            <IonButton onClick={handleSubmit} color="primary"  expand="block" fill="solid">
-              Submit
-            </IonButton>
+          <IonButton onClick={handleSubmit} color="primary" expand="block" fill="solid">
+            Submit
+          </IonButton>
         </div>
       </IonContent>
       <IonToast
